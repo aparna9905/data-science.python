992eca47a67d6f1a235231b74a579bdd01365e1f
aparna9905 <aparnakumari0522@gmail.com>
2026-02-13 10:14:58 +0530

    Clean up library management project.py
    Removed unnecessary comments and whitespace.

--- library management project.py
+++ library management project.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import re
 
@@ -192,17 +187,8 @@ def main():
             print("Invalid choice!")
 
 
-# Run program
+
 main()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
